edb4c806c15ede1ce8c8a31a8e7a95b5dd3d66e4
iamjessu <me@iamjessu.com>
2013-08-20 22:30:32 +0200

    Add options to list open files in current window
    Also added menu options to set keyboard shortcuts and settings. Also
    updated readme.md.

--- OpenFileList.py
+++ OpenFileList.py
@@ -1,9 +1,31 @@
 import sublime, sublime_plugin
 import os.path
 
-class OpenFileListCommand(sublime_plugin.WindowCommand):
-    def run(self):
+# Get the views according to the 'list_mode' argument 
+# on key bindings file (aka keyboard shorcuts)
+def getViews(self, list_mode):
+    if list_mode == "active_group":
         views = self.window.views_in_group(self.window.active_group())
+
+    if list_mode == "window":
+        views = self.window.views()
+
+    return views
+
+class OpenFileListCommand(sublime_plugin.WindowCommand):
+    settings = None
+    def run(self, list_mode):
+        # Load settings.
+        if self.settings == None:
+            self.settings = sublime.load_settings("OpenFileList.sublime-settings")
+
+        views = getViews(self, list_mode)
+
+        # Fallback: if current group has no views,
+        # then list all views in current window
+        if len(views) < 1 and self.settings.get('list_by_window_fallback') == True:
+            views = getViews(self, "window")
+
         names = []
         for view in views:
             name = view.file_name() or view.name() or 'untitled'
--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-open-file-list
+OpenFileList
 ==============
 
-Sublime Text 2 plugin for listing open files in the same group
+Sublime Text 2 plugin for listing open files in the same group or the same window.
+
+
+Keyboard Shortcuts
+-------
+
+By default, by pressing the keyboard shortcuts <kbd>Alt+Ctrl+l</kbd> or <kbd>Alt+Ctrl+Shift+l</kbd> will open a panel with the list of the open files in the current group or in the current window, respectibly. You may change these shortcuts by adding them on your user's key bindings file.
+
+
+Settings
+-------
+
+By default, the setting <kbd>list_by_window_fallback</kbd> is set to <kbd>true</kbd> to display a list of all the open files in the current window if there's no open file on the current group. You may set this value to <kbd>false</kbd> to avoid this, and no files list will be shown.
+
+
+Credits
+-------
+
+**Created by**
+
+Kohichi Aoki - [GitHub](https://github.com/drikin) | [drikin.com](http://drikin.com/)
+
+**Contributors:**
+
+Jesus Leon - [GitHub](https://github.com/iamjessu) | [Twitter](https://twitter.com/iamjessu)
